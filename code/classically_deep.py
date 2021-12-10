c08fb93fbd9aae184f25aedd042e17e6cbeabe7f
mason <masontheburke@gmail.com>
2021-12-09 22:04:06 -0500

    final push 12/9

--- code/classically_deep.py
+++ code/classically_deep.py
@@ -4,43 +4,45 @@ from intervaltree import Interval,IntervalTree
 import os
 import torch
 import random
-from model import DistributionLearner,QuantityLearner
-#import play
+from model import DistributionLearner
+import midiutil
+import matplotlib.pyplot as plt
 
 DEFAULT_HZ = 44100												# audio play rate
 STEP_SIZE = 2205												# how often to sample (currently every 0.05 sec)
+																	# should be a factor of DEFAULT_HZ!
 NUM_SEC = 60													# how many seconds to survey from each song
 NUM_NOTES = 128													# number of possible MIDI notes
 SAMPLES_PER_SONG = int((DEFAULT_HZ / STEP_SIZE) * NUM_SEC)		# number of samples to take per song
-NUM_SONGS = 50													# number of songs to train on
-NUM_NOTES_OUTPUT = 3											# max number of notes to have in output
+NUM_SONGS = 300													# number of songs to train on
+NUM_NOTES_OUTPUT = 3											# max number of notes to have in one line of output
 
-def train(dist_model, quant_model, inputs, labels):
+def train(model, inputs, labels):
 	"""
 	Train RNN with one epoch.
 
 	Inputs:
 	- model: a RNN instance.
 	- input: all of the song data in one np array
+	- labels: all of the song labels in one np array (inputs shifted a timestep)
 
 	Returns:
-	None
+	a list of losses
 	"""
 
-	dist_optimizer = torch.optim.Adam(dist_model.parameters(), lr = dist_model.learning_rate)
-	quant_optimizer = torch.optim.Adam(quant_model.parameters(), lr = quant_model.learning_rate)
+	# declare optimizer and loss_function
+	optimizer = torch.optim.Adam(model.parameters(), lr = model.learning_rate)
+	loss_function = torch.nn.BCELoss()
 
-	loss_function = torch.nn.MSELoss()
+	# setup loss list and initial hidden state
+	loss_list = []
+	hidden_state = torch.randn(model.num_layers, model.batch_size, model.hidden_size)
 
-	dist_hidden_state = torch.randn(dist_model.num_layers, dist_model.batch_size, dist_model.hidden_size)
-	quant_hidden_state = torch.randn(quant_model.num_layers, quant_model.batch_size, quant_model.hidden_size)
-
-	w = dist_model.window_size
-	b = dist_model.batch_size
+	w = model.window_size
+	b = model.batch_size
 	p = w * b
 
-
-	max = int(inputs.shape[0]/ (dist_model.window_size * dist_model.batch_size))
+	max = int(inputs.shape[0]/ p)
 
 	for i in range(max):
 		print(f"training batch \033[0;33m{i + 1}\033[0m/\033[0;32m{max}\033[0;0m", end = "\r")
@@ -49,58 +51,39 @@ def train(dist_model, quant_model, inputs, labels):
 		inputs_batch = torch.as_tensor(inputs[i * p: (i + 1) * p, :])
 		labels_batch = torch.as_tensor(labels[i * p: (i + 1) * p, :])
 
-		dist_inputs_batch = torch.reshape(inputs_batch, (b, w, dist_model.input_size))
-		dist_labels_batch = torch.reshape(labels_batch, (b, w, dist_model.input_size))
-
-		quant_inputs_batch = torch.as_tensor(np.count_nonzero(inputs_batch, axis = 1))
-		quant_labels_batch = torch.as_tensor(np.count_nonzero(labels_batch, axis = 1))
-
-		quant_inputs_batch = torch.reshape(quant_inputs_batch, (b, w, quant_model.hidden_size))
-		quant_labels_batch = torch.reshape(quant_labels_batch, (b, w, quant_model.hidden_size))
-
-
-		############################## DIST_MODEL ##############################
+		inputs_batch = torch.reshape(inputs_batch, (b, w, model.input_size))
+		labels_batch = torch.reshape(labels_batch, (b, w, model.input_size))
 
 		# allow us to update the hidden state without pytorch complaining
-		dist_hidden_state = dist_hidden_state.detach()
+		hidden_state = hidden_state.detach()
 
 		# compute probabilities
-		dist_output, dist_hidden_state = dist_model.call(dist_inputs_batch, dist_hidden_state)
+		output, hidden_state = model.call(inputs_batch, hidden_state)
 
 		# compute loss
-		dist_loss = loss_function(dist_output, dist_labels_batch)
+		loss = loss_function(output, labels_batch)
 
 		# zero optimizers
-		dist_model.zero_grad()
+		model.zero_grad()
 
 		# apply loss
-		dist_loss.backward()
-		dist_optimizer.step()
+		loss.backward()
+		optimizer.step()
 
-		############################## QUANT_MODEL #############################
+		loss_list.append(loss)
 
-		# allow us to update the hidden state without pytorch complaining
-		quant_hidden_state = quant_hidden_state.detach()
-
-		# compute probabilities
-		quant_output, quant_hidden_state = quant_model.call(quant_inputs_batch, quant_hidden_state)
-
-		# compute loss
-		quant_loss = loss_function(quant_output, quant_labels_batch)
-
-		# zero optimizers
-		quant_model.zero_grad()
-
-		# apply loss
-		quant_loss.backward()
-		quant_optimizer.step()
-
-		i += 1
 
 	print(f"training batch \033[0;32m{max}\033[0m/\033[0;32m{max}\ntraining complete!\033[0m", end = "\n")
 
+	return loss_list
 
-def organize_song(id, length, song_info):
+
+def organize_song(length, song_info):
+	"""
+		length: the length of the current song
+		song_info: all of the note data and metadata, store in an IntervalTree
+
+	"""
 
 	# data stores the number of samples + 1,
 	#   where the first will be stripped to form labels
@@ -108,7 +91,10 @@ def organize_song(id, length, song_info):
 	data = np.zeros((SAMPLES_PER_SONG + 1, NUM_NOTES))
 
 	# randomize the starting position of the song
-	start_point = random.randint(0, length - (DEFAULT_HZ * NUM_SEC + STEP_SIZE))
+	start_point = length - (DEFAULT_HZ * NUM_SEC + STEP_SIZE)
+	if start_point < 0:
+		start_point = 0
+	start_point = random.randint(0, start_point)
 
 	# looping through every single time step, getting a data point every STEP_SIZE
 	for timestep in range(start_point, start_point + (DEFAULT_HZ * NUM_SEC) + STEP_SIZE, STEP_SIZE):
@@ -124,49 +110,94 @@ def organize_song(id, length, song_info):
 	return data[:-1], data[1:]
 
 
-"""
-start_notes: array of starter notes [num_notes x model.hidden_size]
-length_song: the length of the generated sequence after the starting notes
+def generate_song(model, start_notes, length_song):
+	"""
+	start_notes: array of starter notes [num_notes x model.hidden_size]
+	length_song: the length of the generated sequence after the starting notes
 
-"""
-def generate_song(dist_model, quant_model, start_notes, length_song):
+	"""
 
-	output_song = np.zeros((length_song, dist_model.hidden_size))
-	dist_hidden_state = None
-	quant_hidden_state = None
+	# we'll fill in output_song with the seed followed by the output song
+	output_song = np.zeros((length_song, model.hidden_size))
+	hidden_state = None
 
 	# generate hidden state with starter sequence
 	for i in range(start_notes.shape[0]):
 		output_song[i] = start_notes[i]
-		current_input = torch.reshape(torch.as_tensor(start_notes[i]), (1, 1, dist_model.hidden_size))
-		next_notes, dist_hidden_state = dist_model.call(current_input, dist_hidden_state)
-
-		next_number, quant_hidden_state = quant_model.call(current_input, quant_hidden_state)
+		current_input = torch.reshape(torch.as_tensor(start_notes[i]), (1, 1, model.hidden_size))
+		next_notes, hidden_state = model.call(current_input, hidden_state)
 
 	# generate song
 	for i in range(start_notes.shape[0], length_song):
-		next_notes = torch.reshape(next_notes, (1, 1, dist_model.hidden_size))
-		next_notes, dist_hidden_state = dist_model.call(next_notes, hidden_state)
-
-		next_number, quant_hidden_state = quant_model.call(next_number, quant_hidden_state)
+		next_notes = torch.reshape(next_notes, (1, 1, model.hidden_size))
+		next_notes, hidden_state = model.call(next_notes, hidden_state)
 
 		next_notes = next_notes.detach()
 
-		next_notes = 1 * (np.argsort(np.argsort(next_notes)) >= next_notes.shape[1] - next_number)
+		# get top NUM_NOTES_OUTPUT many notes from each output line
+		output_notes = 1 * (np.argsort(np.argsort(next_notes)) >= next_notes.shape[1] - NUM_NOTES_OUTPUT)
 
-		output_song[i] = np.array(torch.reshape(next_notes, (1, dist_model.hidden_size)))
+		output_song[i] = np.array(torch.reshape(output_notes, (1, model.hidden_size)))
 
 	return output_song
 
 
+
+def generate_MIDI(song):
+	"""
+		song: a song output array
+	"""
+
+	# create MIDI file with 3 tracks
+	myMIDI = midiutil.MIDIFile(3)
+
+	# MIDI parameters
+	track = 0
+	channel = 0
+	time = 0
+	tempo = 60
+	duration = 0.05
+	volume = 100
+
+	myMIDI.addTempo(track, time, tempo)
+
+	# iterate through all timesteps in song output
+	for timestep in range(0, song.shape[0]):
+		current_notes = np.nonzero(song[timestep])
+
+		# iterate through all notes at current timestep, adding to MIDI
+		if current_notes[0].size != 0:
+			for note in range(len(current_notes)):
+				pitch = current_notes[0][note]
+
+				myMIDI.addNote(track, channel, pitch, time, duration, volume)
+		time += duration
+
+	# write MIDI to file
+	with open("output.mid", "wb") as output_file:
+		myMIDI.writeFile(output_file)
+
+def visualize_data(loss_list):
+	"""
+		loss_list: a list of model losses from training
+	"""
+
+	x_values = np.arange(0, len(loss_list), 1)
+	y_values = loss_list
+	plt.plot(x_values, y_values)
+	plt.xlabel('Batch')
+	plt.ylabel('Loss')
+	plt.title('Loss per Batch Over Time')
+	plt.grid(True)
+	plt.savefig("loss.png")
 
 def main():
 
 	# suppress messages from torch
 	os.environ["PYTORCH_JIT_LOG_LEVEL"] = "2"
 
+	# set default tensor type
 	torch.set_default_tensor_type(torch.DoubleTensor)
-	torch.autograd.set_detect_anomaly(True)
 
 	# create random seed based on system time
 	random.seed()
@@ -178,9 +209,13 @@ def main():
 	# get ids to iterate through ids
 	ids = list(raw_data.keys())
 
-	save_mode = False
+	save_mode = True
 	assert 1 <= NUM_SONGS <= 330
 
+	# if save_mode is True, then save the data to a file before running the model
+	# This MUST be done on the first running of the project
+	# Every time the song-saving parameters are altered in this file, save_mode
+	# must be re-run
 	if save_mode:
 		i = 0
 		for id in ids:
@@ -191,10 +226,10 @@ def main():
 
 			if i == 0:
 				print(f"chronicling song \033[93m{i + 1}\033[0m/\033[92m{NUM_SONGS}\033[0m", end = "\r")
-				INPUTS, LABELS = organize_song(id, len(audio), labels)
+				INPUTS, LABELS = organize_song(len(audio), labels)
 			else:
 				print(f"chronicling song \033[93m{i + 1}\033[0m/\033[92m{NUM_SONGS}\033[0m", end = "\r")
-				song_inp, song_lab = organize_song(id, len(audio), labels)
+				song_inp, song_lab = organize_song(len(audio), labels)
 				INPUTS = np.vstack((INPUTS, song_inp))
 				LABELS = np.vstack((LABELS, song_lab))
 
@@ -205,7 +240,6 @@ def main():
 		np.savez_compressed(f'data/songs.npz', inputs = INPUTS, labels = LABELS)
 		print("\033[92msong arrays saved successfully!\033[0m")
 
-		return
 
 	# otherwise, load data and create model
 	print("\033[93mloading song array...\033[0m")
@@ -216,21 +250,22 @@ def main():
 	labels = train_data['labels']
 
 	# Get an instance of the two models
-	dist_model = DistributionLearner();
-	quant_model = QuantityLearner();
+	model = DistributionLearner();
 
 	# Train RNN
-	train(dist_model, quant_model, inputs, labels)
+	loss_list = train(model, inputs, labels)
 
+	# visualize losses
+	visualize_data(loss_list)
+
+	# pick random set of notes from input to act as a seed for generation
 	rand = random.randint(0, inputs.shape[0] - 33)
-
 	start_notes = inputs[rand : rand + 33]
 	length_song = 300
 
-	song = generate_song(dist_model, quant_model, start_notes, length_song)
-
-	currently_masoning = 1 * (np.argsort(np.argsort(song_probabilities)) >= song_probabilities.shape[1] - 3)
-	#print(currently_masoning[40])
+	# generate the output song iteratively using trained RNN
+	song = generate_song(model, start_notes, length_song)
+	generate_MIDI(song)
 
 if __name__ == "__main__":
 	main()
--- code/model.py
+++ code/model.py
@@ -18,38 +18,11 @@ class DistributionLearner(torch.nn.Module):
         self.GRU = torch.nn.GRU(self.input_size, self.hidden_size, self.num_layers, batch_first = True)
 
         self.linear = torch.nn.Sequential(torch.nn.Linear(self.hidden_size, self.hidden_size),
-                                                 torch.nn.ReLU(),
-                                                 torch.nn.Linear(self.hidden_size, self.hidden_size),
-                                                 torch.nn.ReLU(),
-                                                 torch.nn.Linear(self.hidden_size, self.hidden_size),
-                                                 torch.nn.ReLU()) # change to torch.nn.Softmax() potentially
-
-    def call(self, inputs, h0):
-        """
-        Performs a forward pass for the RNN
-        input: batch of input data
-        """
-        out, hidden_state = self.GRU(inputs, h0)
-        return self.linear(out), hidden_state
-
-
-
-
-
-class QuantityLearner(torch.nn.Module):
-    def __init__(self):
-        super(QuantityLearner, self).__init__()
-
-        self.window_size = 50
-        self.input_size = 1
-        self.batch_size = 12
-        self.hidden_size = 1
-        self.num_layers = 3
-        self.learning_rate = 0.001
-        self.concurrent_notes = 20
-
-        self.GRU = torch.nn.GRU(self.input_size, self.hidden_size, self.num_layers, batch_first= True)
-        self.linear = torch.nn.Linear(self.hidden_size, self.hidden_size)
+                                          torch.nn.ReLU(),
+                                          torch.nn.Linear(self.hidden_size, self.hidden_size),
+                                          torch.nn.ReLU(),
+                                          torch.nn.Linear(self.hidden_size, self.hidden_size),
+                                          torch.nn.Softmax(dim = 1))
 
     def call(self, inputs, h0):
         """
